dd2d20676404eca0584a554151cefed5cbc54ecd
Aleksandr Narodetskii <aleksandrnarodetskii@MacBook-Pro-Aleksandr.local>
2025-10-16 01:16:01 +0300

    Добавил SimpleTest компонент для диагностики
    - Создал простой компонент с красным фоном
    - Добавил его в самый верх страницы
    - Это поможет понять, рендерятся ли компоненты вообще

---
+++ src/components/SimpleTest.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SimpleTest = () => {
+  return (
+    <div style={{ 
+      backgroundColor: 'red',
+      color: 'white',
+      padding: '20px',
+      fontSize: '24px',
+      margin: '20px 0'
+    }}>
+      ПРОСТОЙ ТЕСТ РАБОТАЕТ!
+    </div>
+  );
+};
+
+export default SimpleTest;
